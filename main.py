94ff8593d5bab464227233d7b135f1caac0cf7ac
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2020-12-10 00:50:48 +0200

    Причесал нейминг. Умеем убивать врагов

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ from math import sqrt, sin
 BLACK = (0, 0, 0)
 MAX_MONSTERS_COUNT = 20
 BASE_PATH = os.path.dirname(__file__)
-base_path = BASE_PATH
-screen_size = 1000
+
+NPC_SPEED        = 0.05
+PROJECTILE_SPEED = 1
+
+BULLET = lambda pos: pygame.draw.circle(screen, BLACK, pos, 2)
 
 MONSTERS = [
     'Hitman 1/hitman1_silencer.png',
@@ -17,63 +20,62 @@ MONSTERS = [
     'Robot 1/robot1_silencer.png'
 ]
 
-def load_sprite(name, x = 0, y = 0):
-    img = os.path.join(BASE_PATH, 'resources', 'PNG', name)
-    return { 'x': x, 'y': y,
-             'dx': 0, 'dy': 0,
-             'rotate': 0,
-             'image': pygame.image.load(img) }
+### Sprite
 
-def get_monster_next_position(monster, player_position, timestamp):
-    update_modifier = 1 if timestamp % 5 == 0 else 0
-    current_monster_position = (monster['x'], monster['y'])
-    new_x_position = 0
-    if current_monster_position[0] < player_position[0]:
-        new_x_position = current_monster_position[0] + 1 * update_modifier
-    else:
-        new_x_position = current_monster_position[0] - 1 * update_modifier
-    new_y_position = 0
-    if current_monster_position[1] < player_position[1]:
-        new_y_position = current_monster_position[1] + 1 * update_modifier
-    else:
-        new_y_position = current_monster_position[1] - 1 * update_modifier
-
-    return (new_x_position, new_y_position)
-
-def get_start_position():
-    edge = randint(1, 4)
-    if edge == 1:
-        return (0, randint(0, screen_size))
-    elif edge == 2:
-        return (randint(0, screen_size), 0)
-    elif edge == 3:
-        return (screen_size, randint(0, screen_size))
-    elif edge == 4:
-        return (randint(0, screen_size), screen_size)
-
-timestamp = 0
-screen = pygame.display.set_mode([screen_size, screen_size])
-
-def get_bullet(shooter):
-    bullet = dict()
-    bullet['target'] = pos
-    bullet['start_position'] = shooter
-    bullet['current_position'] = shooter
-    return bullet
-
-def update_bullets_position(bullets):
-    bullet_speed = 3
-    for bullet in bullets:
-        dx = bullet['target'][0] - bullet['start_position'][0]
-        dy = bullet['target'][1] - bullet['start_position'][1]
-        dist = sqrt(dx ** 2 + dy ** 2)
-        bullet['current_position'] = (bullet['current_position'][0] + (dx / dist) * bullet_speed, bullet['current_position'][1] + (dy / dist) * bullet_speed)
-    return bullets
-
-def rotate_angle(dst_position, sprite):
+def sprite_load(x = 0, y = 0, image = None, draw_fn = None):
     """
-    Определяет угол, на который нужно повернуть спрайт, чтобы тот был ориентирован
-    в определенную точку.
+    Создает спрайт по позиции и имени картинки. Загружая текстуру из
+    resources/PNG. Вместо текстуры можно использовать draw_fn - функцию, которая
+    принимает позицию и рисует что-то на экране.
+    """
+    if image:
+        image = os.path.join(BASE_PATH, 'resources', 'PNG', image)
+        image = pygame.image.load(image)
+
+    return { 'x': x, 'y': y, 'rotate': 0,
+             'image':  image, 'draw_fn': draw_fn }
+
+def sprite_position(sprite):
+    """
+    Метод для удобного получения позиции спрайта в виде tuple. Tuples
+    неизменяемые и их удобно передавать в другие методы. Но позицию спрайта
+    удобно держать отдельными полями в dict.
+    """
+    return (sprite['x'], sprite['y'])
+
+def sprite_draw(sprite):
+    """
+    Рисует спрайт на канвасе в указанной точке. Спрайт может быть картинкой.
+    Может быть функцией,которая что-то нарисует. В этом случае смотрим
+    содержимое по ключу 'draw_fn'. Функция принимает единственный аргумент:
+    position.
+    """
+    image = sprite['image']
+    draw_fn = sprite['draw_fn']
+    position = sprite_position(sprite)
+
+    if image:
+        image_center = image.get_rect(center = position).center
+        rotated_image = pygame.transform.rotate(image, sprite['rotate'])
+        rorated_rect = rotated_image.get_rect(center = image_center)
+        screen.blit(rotated_image, rorated_rect.topleft)
+    elif draw_fn:
+        draw_fn(position)
+
+def sprite_box(sprite):
+    """
+    Возвращает bounding box спрайта
+    """
+    image = sprite['image']
+    position = sprite_position(sprite)
+    image_center = image.get_rect(center = position).center
+    rotated_image = pygame.transform.rotate(image, sprite['rotate'])
+    return rotated_image.get_rect(center = image_center)
+
+def sprite_aim(sprite, dst_position):
+    """
+    Определяет угол, на который нужно повернуть спрайт, чтобы тот был
+    ориентирован в определенную точку.
     """
     position = (sprite['x'], sprite['y'])
     vec = (dst_position[0] - position[0], dst_position[1] - position[1])
@@ -86,73 +88,143 @@ def rotate_angle(dst_position, sprite):
 
     cos_a = mul / len
     sign = 0 if vec[1] == 0 else vec[1] / abs(vec[1])
-    return sign * math.acos(cos_a) * 180 / math.pi
+    return -1  * sign * math.acos(cos_a) * 180 / math.pi
 
+### NPC
+
+def npc_update(t, npc, player):
+    """
+    Обновляет позицию NPC так, чтобы тот двигался в сторону игрока. Так же
+    поворачивает NPC в этом направлении.
+    """
+    player_position = sprite_position(player)
+    npc_position    = sprite_position(npc)
+
+    dx = player_position[0] - npc_position[0]
+    dy = player_position[1] - npc_position[1]
+    r  = sqrt(dx ** 2 + dy ** 2)
+    dr = t * NPC_SPEED
+
+    dx0 = dx / r * dr
+    dy0 = dy / r * dr
+
+    npc['x'] = npc['x'] + dx0
+    npc['y'] = npc['y'] + dy0
+
+    npc['rotate'] = sprite_aim(npc, player_position)
+
+def npc_spawn_position():
+    edge = randint(1, 4)
+
+    if edge == 1:
+        return (0, randint(0, screen_size))
+    elif edge == 2:
+        return (randint(0, screen_size), 0)
+    elif edge == 3:
+        return (screen_size, randint(0, screen_size))
+    elif edge == 4:
+        return (randint(0, screen_size), screen_size)
+
+### Projectiles
+
+def projectile_spawn(player, aim, draw_fn):
+    x, y = sprite_position(player)
+
+    dx = aim[0] - x
+    dy = aim[1] - y
+    r  = sqrt(dx ** 2 + dy ** 2)
+
+    projectile = sprite_load(x = x, y = y, draw_fn = draw_fn)
+    projectile['dx'] = dx / r
+    projectile['dy'] = dy / r
+
+    return projectile
+
+screen_size = 1000
+screen      = pygame.display.set_mode([screen_size, screen_size])
+
+# Количество тиков со старта на последнем кадре
+ticks_last  = 0
+# Обновляем игру пока True
+running     = True
+# Список объектов в мире. Наш основной список для отрисовки
+world       = []
+# Список NPC. Нужен для того чтобы обновлять NPC
+npcs        = []
+# Список Projectiles. Все что вылетает из пушек.
+projectiles = []
+# Игрок
+player      = sprite_load(image = 'Woman Green/womanGreen_machine.png',
+                          x = screen_size / 2, y = screen_size / 2)
+world.append(player)
 
 pygame.init()
-screen_size = 1000
-
-running = True
-monster_positions = dict()
-alive_monsters = []
-bullets = []
-
-shooter = load_sprite('Woman Green/womanGreen_machine.png',
-                      x = screen_size / 2, y = screen_size / 2)
-
-world = []
-world.append(shooter)
-
 while running:
+    mouse_position  = pygame.mouse.get_pos()
+    ticks_current   = pygame.time.get_ticks()
+    ticks_delta     = ticks_current - ticks_last
+    ticks_last      = ticks_current
+
     # Did the user click the window close button?
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            bullets.append(get_bullet((shooter['x'], shooter['y'])))
+            projectile = projectile_spawn(player, mouse_position, BULLET)
+            projectiles.append(projectile)
+            world.append(projectile)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_d]:
-        shooter['x'] = shooter['x'] + 1
+        player['x'] = player['x'] + 1
     elif keys[pygame.K_a]:
-        shooter['x'] = shooter['x'] - 1
+        player['x'] = player['x'] - 1
     elif keys[pygame.K_w]:
-        shooter['y'] = shooter['y'] - 1
+        player['y'] = player['y'] - 1
     elif keys[pygame.K_s]:
-        shooter['y'] = shooter['y'] + 1
+        player['y'] = player['y'] + 1
 
-    shooter['rotate'] = rotate_angle(pygame.mouse.get_pos(), shooter)
+    player['rotate'] = sprite_aim(player, pygame.mouse.get_pos())
 
+    if len(npcs) < MAX_MONSTERS_COUNT:
+        spawn_x, spawn_y = npc_spawn_position()
+        npc_image = MONSTERS[randint(0, len(MONSTERS) - 1)]
+        monster = sprite_load(image = npc_image, x = spawn_x, y = spawn_y)
+        npcs.append(monster)
+        world.append(monster)
+
+    remove_projectiles = []
+    screen_box = pygame.Rect(0, 0, screen_size, screen_size)
+    for projectile in projectiles:
+        projectile['x'] = projectile['x'] + projectile['dx'] * PROJECTILE_SPEED
+        projectile['y'] = projectile['y'] + projectile['dy'] * PROJECTILE_SPEED
+
+        if not screen_box.collidepoint(projectile['x'], projectile['y']):
+            remove_projectiles.append(projectile)
+
+    for projectile in remove_projectiles:
+        projectiles.remove(projectile)
+        world.remove(projectile)
+
+    dead_npcs = []
+    for npc in npcs:
+        npc_update(ticks_delta, npc, player)
+        # Проверяем не хитнул ли нас кто
+        for projectile in projectiles:
+            if sprite_box(npc).collidepoint(sprite_position(projectile)):
+                if npc not in dead_npcs:
+                    dead_npcs.append(npc)
+
+    for dead in dead_npcs:
+        npcs.remove(dead)
+        world.remove(dead)
+
+    ##### Draw
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-    if len(alive_monsters) < MAX_MONSTERS_COUNT:
-        spawn_x, spawn_y = get_start_position()
-        monster = load_sprite(MONSTERS[randint(0, len(MONSTERS) - 1)], x = spawn_x, y = spawn_y)
-        alive_monsters.append(monster)
-        world.append(monster)
-
-    bullets = update_bullets_position(bullets)
-    for bullet in bullets:
-        pygame.draw.circle(screen, BLACK, bullet['current_position'], 2)
-
-    for monster in alive_monsters:
-        player_position = (shooter['x'], shooter['y'])
-        monster_position = get_monster_next_position(monster, player_position, timestamp)
-        monster['x'] = monster_position[0]
-        monster['y'] = monster_position[1]
-        monster['rotate'] = rotate_angle((shooter['x'], shooter['y']), monster)
-
     for e in world:
-        image = e['image']
-        rotated_image = pygame.transform.rotate(image, e['rotate'])
-        rorated_rect = rotated_image.get_rect(center = image.get_rect(center = (e['x'], e['y'])).center)
-
-        screen.blit(rotated_image, rorated_rect.topleft)
-
-    # Flip the display
-    timestamp = (timestamp + 1) % 5
+        sprite_draw(e)
 
     pygame.display.flip()
 
