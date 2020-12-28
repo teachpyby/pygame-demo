044286c90ceb6129f1545ca066b47ad1256200be
baydet <evsyuchenya@targetprocess.com>
2020-12-28 23:01:18 +0300

    add gamover

--- snake.py
+++ snake.py
@@ -9,15 +9,19 @@ from math import sqrt, sin
 running = True
 
 ticks_last = 0
-nextDirection = 0
+move_direction = 0
 
 pygame.init()
+pygame.font.init()
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
 
 # directions
 # 0 - up
 # 1 - left
 # 2 - down
 # 3 - right
+
 
 # return if snake alive or not
 # increase snake if snake eat an apple
@@ -27,12 +31,17 @@ pygame.init()
 def move_snake(snake, direction, apple):
     return (snake, True, apple)
 
+def get_apple():
+    return (screen_size / 2, screen_size / 2)
+
 pixel_size = 20
 screen_size = 1000
 pixel_screen_size = screen_size / pixel_size
 
 snake = [(pixel_screen_size / 2, pixel_screen_size - 1)]
 screen      = pygame.display.set_mode([screen_size, screen_size])
+apple = None
+is_alive = False
 
 while running:
     mouse_position  = pygame.mouse.get_pos()
@@ -47,19 +56,32 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        nextDirection = 0
+        move_direction = 0
     elif keys[pygame.K_LEFT]:
-        nextDirection = 1
+        move_direction = 1
     elif keys[pygame.K_DOWN]:
-        nextDirection = 2
+        move_direction = 2
     elif keys[pygame.K_RIGHT]:
-        nextDirection = 3
-
+        move_direction = 3
 
     screen.fill((255, 255, 255))
 
-    for pixel in snake:
-        pygame.draw.rect(screen, (0, 0, 0), (pixel[0] * pixel_size, pixel[1] * pixel_size, pixel_size, pixel_size) )
+    if is_alive:
+        res = move_snake(snake, move_direction, apple)
+        snake = res[0]
+        is_alive = res[1]
+        apple = res[2]
+
+    if apple is None:
+        apple = get_apple()
+
+    if is_alive:
+        for pixel in snake:
+            pygame.draw.rect(screen, (0, 0, 0), (pixel[0] * pixel_size, pixel[1] * pixel_size, pixel_size, pixel_size) )
+    else:
+        game_over_text = myfont.render(f'Game Over. Your score is {len(snake)}', False, (0, 0, 0))
+        screen.blit(game_over_text,(screen_size/2 - game_over_text.get_width() / 2, screen_size/2))
+        screen.blit(game_over_text,(screen_size/2 - game_over_text.get_width() / 2, screen_size/2))
 
     pygame.display.flip()
 
